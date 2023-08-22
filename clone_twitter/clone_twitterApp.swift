67e66cca4bb2d38895780142c8b6426a53f06d19
Burak Kızılay <burak.kizilay@westerops.com>
2023-08-22 19:59:09 +0300

    Initial Commit

---
+++ clone_twitter/clone_twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  clone_twitterApp.swift
+//  clone_twitter
+//
+//  Created by Burak on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct clone_twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
