8d599b83b37a9a15566dfff0e30dfa8f99de3500
Burak Kızılay <burak.kizilay@westerops.com>
2023-08-23 11:47:08 +0300

    post detail view created

--- clone_twitter/ContentView.swift
+++ clone_twitter/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView{
+            ForEach(0..<100){_ in
+                VStack(alignment: .leading, spacing: 18){
+                    HStack{
+                        Image("user").resizable().aspectRatio( contentMode: .fit).frame(width: 48.0).cornerRadius(100)
+                        VStack(alignment: .leading){
+                            Text("Burak Kızılay").fontWeight(.bold)
+                            Text("@devburakkizilay")
+                        }
+                        
+                    }
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    
+                    HStack(spacing: 32){
+                      
+                        Text("32 Retweet")
+                        
+                        Text("34K Like")
+                    
+                        Text("99 Quote")
+                    
+                    }
+                    
+                    HStack(alignment: .center, spacing: 32){
+                
+                        Image(systemName: "heart")
+                        Image(systemName: "square.and.arrow.up")
+                        Image(systemName: "heart")
+                        Image(systemName: "square.and.arrow.up")
+
+                    }
+         
+                }.padding(16)
+                Divider()
+            }
         }
-        .padding()
+        
+        
     }
 }
 
